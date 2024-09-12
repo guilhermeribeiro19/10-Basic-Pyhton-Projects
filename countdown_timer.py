7b192808dde4f7f2613d0693df7534c6065eec55
Guilherme Ribeiro <guilhermefribeiro8@gmail.com>
2024-09-12 16:17:00 +0000

    final countdown changes

--- countdown_timer.py
+++ countdown_timer.py
@@ -4,17 +4,17 @@ print("Welcome to a Countdown Timer\n")
 
 def countdown():
     seconds = int(input("Enter the amount of seconds: "))
-    print("\n Countdown Starts Now:")
+    print("\nCountdown Starts Now:")
 
-    while seconds != 0:
-        print('\b'*len(str(seconds)), end='')
+    while seconds > 0:
+        print(f"\r{seconds} seconds remaining", end='')
         time.sleep(1)
         seconds -= 1
     print("\n CountDown has ended\n")
 
 
 while True:
-    choice = chr(input("Do you want to set a timer: [y] / [n]"))
+    choice = input("Do you want to set a timer: (y/n)  ")
 
     if choice == "y":
         countdown()
