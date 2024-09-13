aaf60dbcd662e4e23840689f327c2ba33fc1b1f5
Guilherme Ribeiro <guilhermefribeiro8@gmail.com>
2024-09-13 11:57:16 +0000

    first changes

---
+++ number_guessing.py
@@ -0,0 +1,44 @@
+import random
+
+def get_random_number():
+    number = random.randint(1,100)
+    return number
+
+print("\nWelcome to the Try to Guess the Number Game!")
+
+
+
+while True:
+    choice = input("\nDo you want to Guess the Number?  ")
+
+    number_to_guess = get_random_number()
+    chances = 5
+    if choice == "y":
+
+        while chances > 0:
+            print(f"\nYou have {chances} chances to get it right")
+
+            guess = int(input("\nChoose a number between 1 and 100:  "))
+
+            if guess == number_to_guess:
+                print("You got the number right!")
+                break
+
+            elif guess < number_to_guess:
+                print("The number you choose is lower than the number we choose, try again...")
+
+            elif guess > number_to_guess:
+                print("The number you choose is higher than the number we choose, try again...")
+
+            chances -= 1
+
+        print("You lost, feel free to try again!")
+
+    elif choice == "n":
+        print("\nExiting...")
+        break
+
+    else:
+        print("\nSomething went wrong, please try again")
+        break
+        
